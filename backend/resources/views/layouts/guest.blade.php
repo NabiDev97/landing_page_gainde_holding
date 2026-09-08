1608ab27b26d12c62c83d6362ae0999f5e7cb2fe
nabidev <mougaye1225@gmail.com>
2026-09-08 10:39:36 +0000

    fix: load built Vite assets via manifest for guest layout (secure_asset fallback)

--- backend/resources/views/layouts/guest.blade.php
+++ backend/resources/views/layouts/guest.blade.php
@@ -12,7 +12,20 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @php
+            $manifestPath = public_path('build/manifest.json');
+        @endphp
+        @if(file_exists($manifestPath))
+            @php $manifest = json_decode(file_get_contents($manifestPath), true); @endphp
+            @if(isset($manifest['resources/css/app.css']['file']))
+                <link rel="stylesheet" href="{{ secure_asset('build/'.$manifest['resources/css/app.css']['file']) }}">
+            @endif
+            @if(isset($manifest['resources/js/app.js']['file']))
+                <script type="module" src="{{ secure_asset('build/'.$manifest['resources/js/app.js']['file']) }}"></script>
+            @endif
+        @else
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @endif
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
